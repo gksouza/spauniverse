3cc26cefede919c3bf773ff101d16adafbb7d3cd
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-01-09 17:07:42 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 import { Router } from './router.js'
 
 const router = new Router()
-  router.add("/","/pages/home.html")
-  router.add("/universe","/pages/universe.html")
-  router.add("/exploration","/pages/exploration.html")
-  router.add(404,"/pages/404.html")
+  router.add("/", "/pages/home.html")
+  router.add("/universe", "/pages/universe.html")
+  router.add("/exploration", "/pages/exploration.html")
+  router.add(404, "/pages/404.html")
 
   router.handle()
 
