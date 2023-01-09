2d80642842f9c9fcfd5230648d62f3b4ac366396
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-01-09 10:07:29 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 import { Router } from './router.js'
-window.addEventListener('scroll', onScroll)
 
 const router = new Router()
   router.add("/","/pages/home.html")
@@ -11,41 +10,4 @@ const router = new Router()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
-
-onScroll()
-
-function onScroll() {
-    showNavOnScroll()
-    showBackToTopButtonOnScroll()
-
-    activateMenuAtCurrentSection(home)
-    activateMenuAtCurrentSection(universe)
-    activateMenuAtCurrentSection(exploration)
-}
-
-function activateMenuAtCurrentSection(section) {
-  const targetLine = scrollY + innerHeight / 2
-  
-  const sectionTop = section.offsetTop
-  const sectionHeight = section.offsetHeight
-
-  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
-
-  const sectionEadAt = sectionTop + sectionHeight
-
-  const sectionEndPassedTargetline = sectionEadAt <= targetLine
-
-      sectionEndPassedTargetline
-
-  const sectionBoundaries =
-  sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline
-  
-  const sectionId = section.getAttribute('id')
-  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
-
-  menuElement.classList.remove ('active')
-  if (sectionBoundaries) {
-      menuElement.classList.add ('active')
-  }
-}
 
