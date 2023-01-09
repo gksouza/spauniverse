3aed95106f584eb0b8428a5424b0e7698250f970
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-01-09 17:03:21 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -14,9 +14,10 @@ export class Router {
 
   handle() {
     const { pathname } = window.location
-    const route = this.routes [pathname] || this.routes [404]
+    const route = this.routes [pathname] || this.routes[404]
+
     fetch(route)
     .then((data) => data.text())
-    .then(html => document.querySelector('#app').innerHTML = html)
+    .then(html => {document.querySelector('#app').innerHTML = html})
   }
 }
