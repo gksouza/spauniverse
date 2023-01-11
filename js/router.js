b86c677b5184c109da1038dfb00811bb4b2be21a
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-01-11 11:58:50 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -18,6 +18,6 @@ export class Router {
 
     fetch(route)
     .then((data) => data.text())
-    .then(html => {document.querySelector('#app').innerHTML = html})
+    .then(html => document.querySelector('#app').innerHTML = html)
   }
 }
